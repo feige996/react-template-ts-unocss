6839eb769cb1f8efd67147a27cc8d4184f6766fc
Burt <burt.lai@kupotech.com>
2024-10-18 15:22:03 +0800

    feat: 处理 alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { sleep } from '@/utils'
+import { useEffect } from 'react'
+async function testAlias() {
+  sleep(1000)
+  console.log('hello feige')
+}
 function App() {
+  useEffect(() => {
+    testAlias()
+  }, [])
+
   return <div className='text-blue-500'>react18+vite5+ts+unocss+svg</div>
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
   plugins: [react(), UnoCSS()],
 })
